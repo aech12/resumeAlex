dabc6ee9f81e96c5d922fee94f91da8bb099bfcf
Alejandro Howez <aech-12@hotmail.com>
2018-11-25 02:54:41 -0300

    Updates

---
+++ precache-manifest.cba3392e2329f4d2893f967100b7f13f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1d2e965488ebecffbe6ec66910e111a",
+    "url": "/resumeAlex/static/media/1.b1d2e965.png"
+  },
+  {
+    "revision": "2a394d7253357858212a",
+    "url": "/resumeAlex/static/css/main.2515e17a.chunk.css"
+  },
+  {
+    "revision": "492f714aca66b01875ac",
+    "url": "/resumeAlex/static/js/1.492f714a.chunk.js"
+  },
+  {
+    "revision": "6aecbc097004da624879",
+    "url": "/resumeAlex/static/js/runtime~main.6aecbc09.js"
+  },
+  {
+    "revision": "7a819bf4853340a206352d2eb5e17b03",
+    "url": "/resumeAlex/static/media/avatar.7a819bf4.png"
+  },
+  {
+    "revision": "2a394d7253357858212a",
+    "url": "/resumeAlex/static/js/main.2a394d72.chunk.js"
+  },
+  {
+    "revision": "e653067985bf089052ff359116d838fe",
+    "url": "/resumeAlex/static/media/2.e6530679.png"
+  },
+  {
+    "revision": "d7c0ec521e2bba5a3ec596873e286f50",
+    "url": "/resumeAlex/static/media/3.d7c0ec52.png"
+  },
+  {
+    "revision": "62b590ca00487624043aae823c67e173",
+    "url": "/resumeAlex/static/media/4.62b590ca.png"
+  },
+  {
+    "revision": "cb3418c646548ca48839579a1776cbbd",
+    "url": "/resumeAlex/static/media/5.cb3418c6.png"
+  },
+  {
+    "revision": "c7251425464e040694f5f48d0db29278",
+    "url": "/resumeAlex/index.html"
+  }
+];
